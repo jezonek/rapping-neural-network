0461c0c24d9b4ad278a739d12d448f9de405fe0c
jezonek <marcinjezon@gmail.com>
2020-01-05 22:40:39 +0100

    Added try/except for text generation

--- text_generation.py
+++ text_generation.py
@@ -8,13 +8,17 @@ def generate_texts_periodically():
     logger.debug("Started text generator")
     collection = prepare_connection_to_db_texts()
     while collection.count_documents({}) < 30:
+
         logger.debug(f'Texts in base: {collection.count_documents({})}')
         lines = main(4, False)
         record = {"text": lines}
         logger.debug("Putting text into db")
         collection.insert_one(record)
-    sleep(60)
+        sleep(60)
 
 if __name__ == '__main__':
     while(True):
-        generate_texts_periodically()
+        try:
+            generate_texts_periodically()
+        except Exception as e:
+            logger.error(e)
